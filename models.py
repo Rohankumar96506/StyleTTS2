0efcad51cc060f433d77a8b9ef56bffbe88b9776
Rohankumar96506 <59435105+Rohankumar96506@users.noreply.github.com>
2025-03-22 00:36:55 +0530

    Update models.py

--- models.py
+++ models.py
@@ -603,7 +603,7 @@ def load_ASR_models(ASR_MODEL_PATH, ASR_MODEL_CONFIG):
         model = ASRCNN(**model_config)
         params = None
         with torch.serialization.safe_globals([getattr]):
-            params = torch.load(model_path, map_location='cpu', weights_only = True)['model']
+            params = torch.load(model_path, map_location='cpu', weights_only = False)['model']
         model.load_state_dict(params)
         return model
 
